95cf5a3cd2ce7c03ba1c97de38601ac7eb180098
LucasGuzylak <lucasguzy11@gmail.com>
2026-07-06 13:44:35 -0700

    Add MIL training loop and model architecture

--- cancer.py
+++ cancer.py
@@ -37,3 +37,52 @@ class MILDataset(Dataset):
 
         return torch.stack(patches), label
     
+class MILModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        resnet = models.resnet18(weights="IMAGENET1K_V1")
+        self.feature_extractor = nn.Sequential(*list(resnet.children())[:-1])
+        
+        self.classifier = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, 1)
+        )
+    
+    def forward(self, bag):
+        bag = bag.squeeze(0)
+        
+        features = self.feature_extractor(bag)
+        features = features.view(features.size(0), -1)
+        
+        patch_scores = self.classifier(features)
+        
+        bag_score = patch_scores.max(dim=0)[0]
+        
+        return bag_score
+    
+def train(model, trainloader, optimizer, loss_fn):
+    model.train()
+    total_loss = 0
+    correct = 0
+    total = 0
+
+    for bags, labels in trainloader:
+        bags = bags.to(device)
+        labels = labels.float().to(device)
+
+        optimizer.zero_grad()
+        outputs = model(bags)
+        loss = loss_fn(outputs.squeeze(), labels.squeeze())
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+        predicted = (outputs.squeeze() > 0.5).float()
+        correct += (predicted == labels.squeeze()).sum().item()
+        total += labels.size(0)
+
+    accuracy = 100 * correct / total
+    return total_loss, accuracy
